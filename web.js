cd343ee1a11b4b6b6c8d3e12133d0ed55fdf8c90
Yeti <Yeti.or@gmail.com>
2013-07-14 06:08:54 +0400

    Add reading from index.html

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+    var indexContent = fs.readFileSync('index.html');
+    //console.log('hi');
+    response.send(indexContent.toString());
+    //response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
